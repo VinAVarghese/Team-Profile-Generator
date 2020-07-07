d4da9443c3781242770d5ff33b5fb9f6ddba6f7f
Vins MacBook <vinavarghese@gmail.com>
2020-07-06 20:34:09 -0700

    inital coding for employee file.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Employee class
+
+const Employee = {
+    name,
+    id,
+    email,
+    getName:function(){
+        
+    },
+    getId:function(){
+
+    },
+    getEmail:function(){
+
+    },
+    getRole:function(){
+        return Employee
+    },
+}

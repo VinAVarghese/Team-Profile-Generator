39728554b1afd1e9afed31d2371abe108374c641
Vins MacBook <vinavarghese@gmail.com>
2020-07-07 17:04:24 -0700

    Employee read, working on Engineer subclass now

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(gitHub){
+        super(name, id, email);
+        this.gitHubUser = gitHubUser
+    }
+    getRole(){
+        return "Engineer"
+    }
+}
+
+module.exports = Engineer
